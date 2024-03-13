1ec23f17b00d036ac95132a247f9d2e45467e4e8
Manel Ariapala <amanelaa@yahoo.com>
2024-03-13 23:30:19 +0100

    Build Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,24 @@
-import React from "react";
-
+import Image from "next/image";
 const Footer = () => {
-  return <div>Footer</div>;
+  const d = new Date();
+  let year = d.getFullYear();
+
+  return (
+    <footer className="bottom-0 w-full pt-8 text-black mb-2 z-10">
+      <div className="flex justify-center items-center gap-3 p-4">
+        <Image
+          src="/logo.svg"
+          alt="logo"
+          width={35}
+          height={35}
+          className="object-contain"
+        />
+        <p className="flex justify-center items-center text-sm gap-1">
+          Ⓒ {year} Must Love Dogs. Terms of Use.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
